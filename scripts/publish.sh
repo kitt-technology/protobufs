1f293ba5c0caab9f15e9baff34e1cf44c88dfcd5
theomjones <theomjones@gmail.com>
2021-02-04 10:44:17 +0000

    feat: update proto files

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -11,5 +11,5 @@ echo 'Committing...';
 git add -A &&
 git commit -m 'feat: update proto files' &&
 git push origin main &&
-git tag -a $NEXT -m "update protobufs $NEXT" &&
+git tag -a "v$NEXT" -m "update protobufs $NEXT" &&
 git push origin $NEXT
